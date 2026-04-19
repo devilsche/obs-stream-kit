6c33809d1839670d77128ec106d5a8096af81737
Dominic Ruschinski <ruschinski@strato.de>
2026-04-20 01:55:12 +0200

    fix(serve): No-Cache-Header auf alle Responses (Dev-Server verhindert veraltete JS/CSS-Caches)

--- serve.py
+++ serve.py
@@ -54,6 +54,10 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "*")
+        # No-Cache: Dev-Server, jedes Mal frisch laden (keine veralteten JS/CSS)
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
         super().end_headers()
 
     def do_OPTIONS(self):
